f7433d2c97b7651daf4369495f3b3dae2ca64701
bobby-22 <nlongn22@icloud.com>
2021-10-31 02:57:51 +0200

    Add typewriter effect (fix using promise)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,24 @@
 <template>
     <div class="container">
         <div class="container-left">
-            <div class="header">Hello</div>
+            <transition name="fade">
+                <div class="header" v-if="message !== '' && isLeft">
+                    {{ message }}<span class="cursor">|</span>
+                </div>
+            </transition>
         </div>
         <div class="container-right">
-            <div class="header">Guten Tag</div>
+            <transition name="fade">
+                <div class="header" v-if="message !== '' && !isLeft">
+                    {{ message }}<span class="cursor">|</span>
+                </div>
+            </transition>
+        </div>
+        <div class="scroll-container">
+            <div class="scroll">Scroll</div>
+            <div class="scroll-icon">
+                <i class="fas fa-long-arrow-alt-down"></i>
+            </div>
         </div>
     </div>
 </template>
@@ -12,11 +26,55 @@
 <script>
 export default {
     name: "Home",
+    data() {
+        return {
+            interval: null,
+            message: "",
+            index: 0,
+            isLeft: true,
+        };
+    },
+    methods: {
+        initTypewriter(string, isLeft) {
+            this.isLeft = isLeft;
+            let stringArray = string.split("");
+            this.message += stringArray[this.index];
+            this.index++;
+        },
+        initFunction(string, boolean) {
+            return new Promise((resolve, reject) => {
+                let interval = setInterval(() => {
+                    this.initTypewriter(string, boolean);
+                    if (this.message === string) {
+                        this.message = "";
+                        this.index = 0;
+                        resolve();
+                        clearInterval(interval);
+                    }
+                }, 225);
+            });
+        },
+    },
+    mounted() {
+        this.initFunction("Hello!", true).then(() =>
+            this.initFunction("Guten Tag!", false).then(() => {
+                this.initFunction("Welcome to my site!", true);
+            })
+        );
+    },
 };
 </script>
 
 <style>
+* {
+    font-family: "Righteous";
+}
+.fade-enter,
+.fade-leave-to {
+    opacity: 0;
+}
 .container {
+    position: relative;
     display: flex;
     min-height: 100vh;
 }
@@ -37,8 +95,37 @@ export default {
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    font-family: "Righteous";
-    font-size: 96px;
     text-align: center;
+    font-size: 96px;
+    transition: 1s;
+}
+.header::after {
+    transition: 1s;
+}
+.header::before {
+    transition: 1s;
+}
+@keyframes blink {
+    0% {
+        opacity: 0;
+    }
+}
+.cursor {
+    color: #cdc9c3;
+    animation: blink 1s steps(2) infinite;
+}
+.scroll {
+    position: absolute;
+    top: 90%;
+    left: 47.5%;
+    transform: rotate(-90deg);
+    color: #fbf7f0;
+}
+.scroll-icon {
+    font-size: 24px;
+    position: absolute;
+    top: 95%;
+    left: 50.2%;
+    color: #d9e4dd;
 }
 </style>
